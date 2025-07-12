f773af42b4b7fb19d6c7f3385db06033919b71a0
obaikoko <jesseobinna7@gmail.com>
2025-07-12 22:38:48 +0100

    updated

--- app/(root)/events/page.tsx
+++ app/(root)/events/page.tsx
@@ -1,12 +1,33 @@
 'use client';
 import EventCard from '@/components/shared/events/event-card';
-import Spinner from '@/components/shared/spinner';
+import { Card, CardContent } from '@/components/ui/card';
 
 import { useGetEventsQuery } from '@/src/features/events/eventApiSlice';
 import Image from 'next/image';
 
 const EventPage = () => {
   const { data, isLoading, isError } = useGetEventsQuery();
+
+  
+    if (isLoading) {
+      return (
+        <div className='p-6'>
+          <Card>
+            <CardContent>Loading events...</CardContent>
+          </Card>
+        </div>
+      );
+    }
+  
+    if (isError || !data) {
+      return (
+        <div className='p-6'>
+          <Card>
+            <CardContent>Failed to load events.</CardContent>
+          </Card>
+        </div>
+      );
+    }
 
   return (
     <>
@@ -22,19 +43,16 @@ const EventPage = () => {
           Upcoming Events
         </h1>
       </div>
-      <div className='min-h-screen bg-gray-100 py-12'>
-        {isLoading && <Spinner />}
+      <div className='min-h-screen  py-12'>
         <div className='container mx-auto px-4 lg:px-8'>
           <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-center'>
-            {isError && (
+            {data.length === 0 ? (
               <h1 className='text-3xl text-center'>
                 No Events Yet, Please Check Back Later.
               </h1>
+            ) : (
+              data.map((event) => <EventCard key={event.id} event={event} />)
             )}
-
-            {data &&
-              !isError &&
-              data.map((event) => <EventCard key={event.id} event={event} />)}
           </div>
         </div>
       </div>
